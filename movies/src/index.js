f32326cbc1881b30d8946a0228b3e2c48550758c
YangQing Li <2217175013@qq.com>
2023-10-17 13:37:51 +0100

    Home Page rendering.

--- movies/src/index.js
+++ movies/src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import HomePage from "./pages/homePage";
-import MovieDetailsPage from './pages/movieDetailsPage'
+
 const sample = {
   adult: false,
   backdrop_path: "/5Iw7zQTHVRBOYpA0V6z0yypOPZh.jpg",
@@ -87,16 +87,9 @@ const movies = [sample, sample, sample, sample, sample, sample, sample];
 
 const App = () => {
   return (
-    <MovieDetailsPage movie={sample} images={images} />
+      <HomePage movies={movies} />
   );
 };
 
 const rootElement = createRoot( document.getElementById("root") )
 rootElement.render(<App />);
-
-const images = [
-  "/kOVEVeg59E0wsnXmF9nrh6OmWII.jpg",
-  "/v1QQKq8M0fWxMgSdGOX1aCv8qMB.jpg",
-  "/2iGN0aKHJYD0xQydlfuCUAcgNbO.jpg",
-  "/rjBwhsOzHKUw2NIOrE7aMqjfe6s.jpg",
-]
