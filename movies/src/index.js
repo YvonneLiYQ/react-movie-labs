9004045dc87db20810f8e0d7065e0e20dd6713b6
YangQing Li <2217175013@qq.com>
2023-10-25 21:08:40 +0100

    Change site header.

--- movies/src/index.js
+++ movies/src/index.js
@@ -1,14 +1,17 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter, Route, Navigate, Routes, Link } from "react-router-dom";
+import { BrowserRouter, Route, Navigate, Routes } from "react-router-dom";
 import HomePage from "./pages/homePage";
 import MoviePage from "./pages/movieDetailsPage";
 import FavoriteMoviesPage from "./pages/favoriteMoviesPage";
 import MovieReviewPage from "./pages/movieReviewPage";
+import SiteHeader from './components/siteHeader';
+
+
 
 const App = () => {
   return (
-    <BrowserRouter>
+    /*<BrowserRouter>
       <ul>
         <li>
           <Link to="/">Home</Link>
@@ -24,9 +27,20 @@ const App = () => {
         <Route path="*" element={ <Navigate to="/" /> } />
         <Route path="/reviews/:id" element={ <MovieReviewPage /> } />
       </Routes>
+    </BrowserRouter>*/
+    <BrowserRouter>
+      <SiteHeader />
+      <Routes>
+        <Route path="/reviews/:id" element={ <MovieReviewPage /> } />
+        <Route path="/movies/favorites" element={<FavoriteMoviesPage />} />
+        <Route path="/movies/:id" element={<MoviePage />} />
+        <Route path="/" element={<HomePage />} />
+        <Route path="*" element={ <Navigate to="/" /> } />
+      </Routes>
     </BrowserRouter>
   );
 };
 
 const rootElement = createRoot( document.getElementById("root") )
 rootElement.render(<App />);
+
